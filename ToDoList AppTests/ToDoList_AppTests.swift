9dbb7bee30b07cf306c91074f3004d6c4f8b2251
William_Tsai <tsaiyuhlin@gmail.com>
2018-01-23 19:01:24 -0800

    Initial Commit

---
+++ ToDoList AppTests/ToDoList_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoList_AppTests.swift
+//  ToDoList AppTests
+//
+//  Created by William Tsai on 1/23/18.
+//  Copyright © 2018 William Tsai. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoList_App
+
+class ToDoList_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
